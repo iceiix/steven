be0a1f1bb69a3d76ad58f9ccd16d73315450f3e1
ice_iix <ice_ix@protonmail.ch>
2018-10-26 19:12:20 -0700

    Disable boxed logger for now to workaround 'expected function, found macro'

--- src/main.rs
+++ src/main.rs
@@ -179,7 +179,8 @@ fn main() {
 
     let proxy = console::ConsoleProxy::new(con.clone());
 
-    log::set_boxed_logger(Box::new(proxy)).unwrap();
+    // TODO: fix error[E0423]: expected function, found macro `log::set_boxed_logger`
+    //log::set_boxed_logger(Box::new(proxy)).unwrap();
     log::set_max_level(log::LevelFilter::Trace);
 
     info!("Starting steven");
