d400c9b87b9ff652b5bbfc69dbd51812dcaadd1d
ice_iix <ice_ix@protonmail.ch>
2018-11-04 07:42:05 -0800

    Add error checking, returning Err<String> on failure; RFC comments

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -115,47 +115,56 @@ fn find_bitstrings(asns: Vec<ASN1Block>, mut result: &mut Vec<Vec<u8>>) {
 fn rsa_public_encrypt_pkcs1(der_pubkey: &[u8], message: &[u8]) -> Result<Vec<u8>, String> {
     // Outer ASN.1 encodes 1.2.840.113549.1.1 OID and wraps a bitstring, find it
     let asns: Vec<ASN1Block> = from_der(&der_pubkey).unwrap();
-    println!("asns = {:?}", asns);
-    for asn in asns.iter() {
-        println!("asn = {:?}", asn);
-    }
     let mut result: Vec<Vec<u8>> = vec![];
     find_bitstrings(asns, &mut result);
+    if result.len() == 0 {
+        return Err("ASN.1 BitString not found in DER encoding of public key".to_string());
+    }
 
     let inner_asn: Vec<ASN1Block> = from_der(&result[0]).unwrap();
-    println!("inner_asn {:?}", inner_asn[0]);
     let (n, e) =
     match &inner_asn[0] {
         ASN1Block::Sequence(_, _, blocks) => {
+            if blocks.len() != 2 {
+                return Err("ASN.1 sequence bad length, expected exactly two blocks in inner Sequence".to_string());
+            }
+
             let n = match &blocks[0] {
-                ASN1Block::Integer(_, _, n) => Some(n),
-                _ => None,
+                ASN1Block::Integer(_, _, n) => n,
+                _ => return Err("ASN.1 Integer modulus not found".to_string()),
             };
 
             let e = match &blocks[1] {
-                ASN1Block::Integer(_, _, e) => Some(e),
-                _ => None,
+                ASN1Block::Integer(_, _, e) => e,
+                _ => return Err("ASN.1 Integer exponent not found".to_string()),
             };
             (n, e)
 
         },
-        _ => (None, None)
+        _ => return Err("ASN.1 Sequence not found".to_string()),
     };
-    let n = n.unwrap();
-    let e = e.unwrap();
-    println!("N=0x{:}", n.to_str_radix(16));
-    println!("e=0x{:}", e.to_str_radix(16));
 
     // PKCS#1 padding https://tools.ietf.org/html/rfc8017#section-7.2.1 RSAES-PKCS1-V1_5-ENCRYPT ((n, e), M)
     let k = n.bits() / 8; // bytes in modulus
-    if k != 1024/8 { panic!("expected 1024-bit modulus"); }
-    println!("k = {}", k);
+    // TODO: is it possible this will be a non-integral value, do we need to handle this case?
+    //if k != 1024/8 { panic!("expected 1024-bit modulus"); }
 
+    /* Steps:
+     *
+     *  1.  Length checking: If mLen > k - 11, output "message too long"
+     *     and stop.
+     */
     if message.len() > k - 11 {
-        panic!("message too long");
+        return Err("PKCS#1 error: message too long".to_string());
     }
 
-    // Fill padding (PS) with _nonzero_ bytes
+    /*
+     * 2.  EME-PKCS1-v1_5 encoding:
+     *
+     *     a.  Generate an octet string PS of length k - mLen - 3
+     *         consisting of pseudo-randomly generated nonzero octets.
+     *         The length of PS will be at least eight octets.
+     */
     let mut padding = vec![1; k - message.len() - 3];
     use rand;
     let mut i = 0;
@@ -164,22 +173,51 @@ fn rsa_public_encrypt_pkcs1(der_pubkey: &[u8], message: &[u8]) -> Result<Vec<u8>
         i += 1;
     }
 
+    /* b.  Concatenate PS, the message M, and other padding to form
+     *     an encoded message EM of length k octets as
+     *
+     *            EM = 0x00 || 0x02 || PS || 0x00 || M.
+     */
     let mut encoded_m = vec![0x00, 0x02];
     encoded_m.append(&mut padding.to_vec());
     encoded_m.append(&mut vec![0x00]);
     encoded_m.extend_from_slice(&message);
-    println!("encoded_m = {:?}", encoded_m);
 
+    /* 3.  RSA encryption:
+     *
+     *     a.  Convert the encoded message EM to an integer message
+     *         representative m (see Section 4.2):
+     *
+     *           m = OS2IP (EM).
+     */
     // OS2IP https://tools.ietf.org/html/rfc8017#section-4.2
     let m = BigInt::from_bytes_be(num::bigint::Sign::Plus, &encoded_m);
 
+    /*     b.  Apply the RSAEP encryption primitive (Section 5.1.1) to
+     *         the RSA public key (n, e) and the message representative m
+     *         to produce an integer ciphertext representative c:
+     *
+     *            c = RSAEP ((n, e), m).
+     */
+    // https://tools.ietf.org/html/rfc8017#section-5.1.1
+    /* 1.  If the message representative m is not between 0 and n - 1,
+     *     output "message representative out of range" and stop.
+     */
+    /* TODO
+    if m < 0 || m > n - 1 {
+        return Err("RSA error: message representative out of range".to_string());
+    }
+    */
+
+    // 2.  Let c = m^e mod n.
     let ciphertext_bigint = m.modpow(&e, &n);
-    // Convert bigint to octet string
+
+    /*     c.  Convert the ciphertext representative c to a ciphertext C
+     *         of length k octets (see Section 4.1):
+     *
+     *            C = I2OSP (c, k).
+     */
     // 4.1. I2OSP https://tools.ietf.org/html/rfc8017#section-4.1
-
-    println!("m = 0x{:}", m.to_str_radix(16));
-    println!("ciphertext = 0x{:}", ciphertext_bigint.to_str_radix(16));
-
     let (_sign, ciphertext) = ciphertext_bigint.to_bytes_be();
 
     return Ok(ciphertext);
