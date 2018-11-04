b33ca718c6c405dbdaec2c380ed2470ba32b5210
ice_iix <ice_ix@protonmail.ch>
2018-11-04 07:55:41 -0800

    Map the ASN.1 error to a String to return it from rsa_public_encrypt_pkcs1() instead of panicking

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -114,15 +114,14 @@ fn find_bitstrings(asns: Vec<ASN1Block>, mut result: &mut Vec<Vec<u8>>) {
 
 fn rsa_public_encrypt_pkcs1(der_pubkey: &[u8], message: &[u8]) -> Result<Vec<u8>, String> {
     // Outer ASN.1 encodes 1.2.840.113549.1.1 OID and wraps a bitstring, find it
-    // TODO: instead of unwrap(), try! and return simple_asn1::ASN1DecodeErr or an ErrorStack
-    let asns: Vec<ASN1Block> = from_der(&der_pubkey).expect("ASN.1 from_der failed to decode public key");
+    let asns: Vec<ASN1Block> = try!(from_der(&der_pubkey).map_err(|err| err.to_string()));
     let mut result: Vec<Vec<u8>> = vec![];
     find_bitstrings(asns, &mut result);
     if result.len() == 0 {
         return Err("ASN.1 BitString not found in DER encoding of public key".to_string());
     }
 
-    let inner_asn: Vec<ASN1Block> = from_der(&result[0]).expect("ASN.1 from_der failed to decode BitString");
+    let inner_asn: Vec<ASN1Block> = try!(from_der(&result[0]).map_err(|err| err.to_string()));
     let (n, e) =
     match &inner_asn[0] {
         ASN1Block::Sequence(_, _, blocks) => {
