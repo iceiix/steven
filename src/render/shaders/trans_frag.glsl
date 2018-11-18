110d1a3adfc256cf1781f9d1914e1348ee80630d
ice_iix <ice_ix@protonmail.ch>
2018-11-18 13:29:12 -0800

    Remove alp accum clamping, only need texelFetch(tcolor, C, 0) to repro

--- src/render/shaders/trans_frag.glsl
+++ src/render/shaders/trans_frag.glsl
@@ -10,13 +10,5 @@ void main() {
     float aa = texelFetch(trevealage, C, 0).r;
     vec4 col = texelFetch(tcolor, C, 0);
 
-    float r = accum.a;
-    accum.a = aa;
-    if (r >= 1.0) {
-        fragColor = vec4(col.rgb, 0.0);
-    } else {
-        vec3 alp = clamp(accum.rgb / clamp(accum.a, 1e-4, 5e4), 0.0, 1.0);
-        fragColor = vec4(col.rgb * r  + alp * (1.0 - r), 0.0);
-    }
-
+    fragColor = vec4(col.rgb, 0.0);
 }
