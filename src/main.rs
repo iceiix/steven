6a9f072e6916463fcb1f5fed75812081b8f9fb85
ice_iix <ice_ix@protonmail.ch>
2018-11-19 07:22:20 -0800

    Add (commented-out) SDL_VIDEO_HIGHDPI_DISABLED window flag to disable HDPI

--- src/main.rs
+++ src/main.rs
@@ -175,6 +175,7 @@ fn main() {
                             .build()
                             .expect("Could not create sdl window.");
     sdl2::hint::set_with_priority("SDL_MOUSE_RELATIVE_MODE_WARP", "1", &sdl2::hint::Hint::Override);
+    //sdl2::hint::set_with_priority("SDL_VIDEO_HIGHDPI_DISABLED", "1", &sdl2::hint::Hint::Override);
     let gl_attr = sdl_video.gl_attr();
     gl_attr.set_stencil_size(0);
     gl_attr.set_depth_size(24);
