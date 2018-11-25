73cec9e09d54919b6f85aa48c43a1e32e2493f23
iceiix <ice_ix@protonmail.ch>
2018-11-25 15:28:21 -0800

    Remove sun, render only moon

--- src/server/sun.rs
+++ src/server/sun.rs
@@ -4,7 +4,6 @@ use crate::render::model;
 use cgmath::{Vector3, Matrix4, Decomposed, Rotation3, Rad, Quaternion};
 
 pub struct SunModel {
-    sun: model::ModelKey,
     moon: model::ModelKey,
     last_phase: i32,
 }
@@ -15,7 +14,6 @@ impl SunModel {
 
     pub fn new(renderer: &mut render::Renderer) -> SunModel {
         SunModel {
-            sun: SunModel::generate_sun(renderer),
             moon: SunModel::generate_moon(renderer, 0),
             last_phase: 0,
         }
@@ -35,19 +33,6 @@ impl SunModel {
         let oy = (time * PI).sin() * 300.0;
 
         {
-            let sun = renderer.model.get_model(self.sun).unwrap();
-            sun.matrix[0] = Matrix4::from(Decomposed {
-                scale: 1.0,
-                rot: Quaternion::from_angle_z(Rad(-(time * PI) as f32)),
-                disp: Vector3::new(
-                    (renderer.camera.pos.x + ox) as f32,
-                    -(renderer.camera.pos.y + oy) as f32,
-                    renderer.camera.pos.z as f32,
-                ),
-            });
-        }
-
-        {
             let moon = renderer.model.get_model(self.moon).unwrap();
             moon.matrix[0] = Matrix4::from(Decomposed {
                 scale: 1.0,
@@ -62,21 +47,7 @@ impl SunModel {
     }
 
     pub fn remove(&mut self, renderer: &mut render::Renderer) {
-        renderer.model.remove_model(self.sun);
         renderer.model.remove_model(self.moon);
-    }
-
-    pub fn generate_sun(renderer: &mut render::Renderer) -> model::ModelKey {
-        let tex = render::Renderer::get_texture(renderer.get_textures_ref(), "environment/sun");
-        renderer.model.create_model(
-            model::SUN,
-            vec![vec![
-                model::Vertex{x: 0.0, y: -SIZE, z: -SIZE, texture_x: 0.0, texture_y: 1.0, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
-                model::Vertex{x: 0.0, y: SIZE, z: -SIZE, texture_x: 0.0, texture_y: 0.0, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
-                model::Vertex{x: 0.0, y: -SIZE, z: SIZE, texture_x: 1.0, texture_y: 1.0, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0},
-                model::Vertex{x: 0.0, y: SIZE, z: SIZE, texture_x: 1.0, texture_y: 0.0, texture: tex.clone(), r: 255, g: 255, b: 255, a: 0, id: 0}
-            ]]
-        )
     }
 
     pub fn generate_moon(renderer: &mut render::Renderer, phase: i32) -> model::ModelKey {
