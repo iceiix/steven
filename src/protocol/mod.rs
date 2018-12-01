ba63b4062667f50763f70d00df9659585c156e8f
ice_iix <ice_ix@protonmail.ch>
2018-11-30 20:10:38 -0800

    Add packet parsing logging

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -786,7 +786,7 @@ impl Serializable for Position {
 
 /// Direction is used to define whether packets are going to the
 /// server or the client.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub enum Direction {
     Serverbound,
     Clientbound,
@@ -794,7 +794,7 @@ pub enum Direction {
 
 /// The protocol has multiple 'sub-protocols' or states which control which
 /// packet an id points to.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub enum State {
     Handshaking,
     Play,
@@ -956,7 +956,9 @@ impl Conn {
             Direction::Serverbound => Direction::Clientbound,
         };
 
+        println!("parsing packet id {:x} direction {:?} in state {:?}", id, dir, self.state);
         let packet = packet::packet_by_id(self.state, dir, id, &mut buf)?;
+        println!("parsed packet = {:?}", packet);
 
         match packet {
             Some(val) => {
