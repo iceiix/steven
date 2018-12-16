7a0558009c4a8204e59d39c2a201e1eedd90d478
ice_iix <ice_ix@protonmail.ch>
2018-12-16 13:01:47 -0800

    Remove the packet logging

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -917,10 +917,6 @@ impl Conn {
             Direction::Serverbound => Direction::Clientbound,
         };
 
-        println!("about to parse packet {:?}", buf);
-        if id == 0x55 {
-            std::fs::File::create("/tmp/p")?.write_all(buf.get_ref())?;
-        }
         let packet = packet::packet_by_id(self.protocol_version, self.state, dir, id, &mut buf)?;
 
         match packet {
