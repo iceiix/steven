39c6308b001d72f42115d3b4daabb89fbdd4415e
ice_iix <ice_ix@protonmail.ch>
2018-12-24 16:52:14 -0800

    Change default variant from 'normal' to ''

--- blocks/src/lib.rs
+++ blocks/src/lib.rs
@@ -156,7 +156,8 @@ macro_rules! define_blocks {
                             $($fname,)*
                         } => {
                             $(return String::from($variant);)*
-                            "normal".to_owned()
+                            //"normal".to_owned()
+                            "".to_owned()
                         }
                     )+
                 }
